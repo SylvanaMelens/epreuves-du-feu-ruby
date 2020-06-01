94c5410ad47c5e48f8a1a0e17795b4c9dee872fd
SylvanaMelens <sylvana.melens@hotmail.com>
2020-06-01 05:22:55 -0700

    add escalier.rb

---
+++ escalier.rb
@@ -0,0 +1,20 @@
+if ARGV.length != 1
+    puts "Please enter only one number of steps"
+    exit
+end
+
+def upstairs(steps)
+    hash = 0
+    space = steps
+    if steps.to_i == 0
+        puts "Please enter a min number of steps" 
+    else
+        steps.times {
+            hash +=1
+            space -=1
+            puts " " * space + "#" * hash 
+        }
+    end
+end
+
+upstairs(ARGV[0].to_i)
