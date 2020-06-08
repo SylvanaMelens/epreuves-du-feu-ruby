56a897daff277c5580f00a896a18724071a14ce8
SylvanaMelens <sylvana.melens@hotmail.com>
2020-06-08 05:48:32 -0700

    modify tri.rb without .sort method

--- tri.rb
+++ tri.rb
@@ -1,3 +1,31 @@
+if (ARGV.length <= 1)
+    puts "Please enter at least 2 arguments to sort : #{__FILE__} [array_of_numbers_to_sort]"
+    exit
+end
 
-arr = ARGV.to_a.sort!.reverse.join(" ")
-puts arr
+
+len = ARGV.length()
+arr = ARGV
+
+flag = true
+
+
+while (flag)
+    flag = false
+    i = 0 
+    while (i < len -1) 
+        if (arr[i] < arr[i + 1])
+            temp = arr[i]
+            arr[i] = arr[i + 1]
+            arr[i + 1] = temp
+            flag = true
+        end
+        i += 1
+    end
+end
+
+puts arr.join(' ')
+
+
+
+
